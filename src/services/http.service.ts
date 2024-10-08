0357269d3ebefd470c88b7ebc31cfb6064e314b2
hqyuh <73837629+hqyuh@users.noreply.github.com>
2024-10-09 01:02:00 +0700

    chore: update http.service 30%

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -14,20 +14,20 @@ import _omitBy from 'lodash/omitBy';
 import { trackPromise } from 'react-promise-tracker';
 
 type TFailedRequests = {
-  resolve: (value: AxiosResponse) => void;
+  resolve: any;
   reject: (value: AxiosError) => void;
   config: AxiosRequestConfig;
   error: AxiosError;
 };
 
-// const MAXIMUM_RETRY_UN_AUTHENTICATION = 5;
+const MAXIMUM_RETRY_UN_AUTHENTICATION = 5;
 
 export default class HttpService {
   private readonly instance: AxiosInstance;
 
-  private readonly failedRequests: TFailedRequests[] = [];
+  private failedRequests: TFailedRequests[] = [];
 
-  private readonly isTokenRefreshing: boolean = false;
+  private isTokenRefreshing = false;
 
   private readonly refreshTokenCount = new Map();
 
@@ -71,14 +71,38 @@ export default class HttpService {
     return result;
   };
 
-  private readonly onResponseError = (error: AxiosError): Promise<AxiosError> => {
+  private readonly onResponseError = async (error: AxiosError): Promise<AxiosError> => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    const originalRequest = error.config!;
+    const { url } = originalRequest;
+
+    if (this.isTokenRefreshing) {
+      return new Promise((resolve, reject) => {
+        if (originalRequest) {
+          this.failedRequests.push({ resolve, reject, config: originalRequest, error });
+        }
+      });
+    }
+
+    const existedRefreshTokenCount = this.refreshTokenCount.get(url) ?? 0;
+
+    if (existedRefreshTokenCount >= MAXIMUM_RETRY_UN_AUTHENTICATION) {
+      window.location.href = '/login';
+
+      return Promise.reject(new Error('Maximum retry attempts exceeded. Redirecting to login.'));
+    }
+
+    this.refreshTokenCount.set(url, existedRefreshTokenCount + 1);
+    this.isTokenRefreshing = true;
+
     const status = error.response?.status;
     // const errorMessage = (error?.toJSON() as any)?.message || 'Error';
 
     switch (status) {
       case HttpStatusCode.UNAUTHORIZED:
-        window.location.href = '/login';
-        break;
+        // window.location.href = '/login';
+
+        return Promise.reject(error);
       case HttpStatusCode.BAD_REQUEST:
       case HttpStatusCode.INTERNAL_SERVER_ERROR:
       case HttpStatusCode.SERVICE_UNAVAILABLE:
@@ -88,7 +112,41 @@ export default class HttpService {
         break;
     }
 
-    return Promise.reject(error);
+    try {
+      const refreshToken = Cookie.get('refresh_token') ?? '';
+      const urlEndpoint = `${22}/api/token/refresh-token`;
+
+      const response = await axios.post(urlEndpoint, { refreshToken });
+
+      const result = response.data;
+      const user = result?.user;
+
+      Cookie.set('access_token', user?.token, { path: '/' });
+      Cookie.set('refresh_token', user?.refreshToken, { path: '/' });
+
+      this.failedRequests.forEach(({ resolve, reject, config }) => {
+        this.instance(config)
+          .then((resHttp) => resolve(resHttp))
+          .catch((errorHttp) => reject(errorHttp));
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (_error: unknown) {
+      this.failedRequests.forEach(({ reject, error: errorFailedRequest }) => reject(errorFailedRequest));
+      this.removeTokenCookie();
+      window.location.href = `${22}`;
+
+      return Promise.reject(error);
+    } finally {
+      this.failedRequests = [];
+      this.isTokenRefreshing = false;
+    }
+
+    if (originalRequest) {
+      return this.instance(originalRequest);
+    }
+
+    return Promise.reject(new Error('Original request is undefined.'));
   };
 
   private setupInterceptorsTo(axiosInstance: AxiosInstance): AxiosInstance {
